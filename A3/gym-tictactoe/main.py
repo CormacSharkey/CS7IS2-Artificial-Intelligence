853cb9d8b30e2ba51127b6babe5c0a558d558b66
CormacSharkey <csharkey@tcd.ie>
2025-03-18 20:57:39 +0000

    cleaning up A1 and setting up A3

---
+++ A3/gym-tictactoe/main.py
@@ -0,0 +1,55 @@
+import random
+
+from gym_tictactoe.env import TicTacToeEnv, agent_by_mark, check_game_status,\
+    after_action_state, tomark, next_mark
+
+
+class BaseAgent(object):
+    def __init__(self, mark):
+        self.mark = mark
+
+    def act(self, state, ava_actions):
+        # For every action in the provided available actions
+        for action in ava_actions:
+            # Get the proposed state of the board after taking the given action
+            nstate = after_action_state(state, action)
+            # Get the game status (victory X, victory O, no-win, still playing) based on the proposed state
+            gstatus = check_game_status(nstate[0])
+
+            # If the game status is a victory
+            if gstatus > 0:
+                # If the current agent's mark is the victory mark
+                if tomark(gstatus) == self.mark:
+                    # Return the current action (ensures victory for the agent)
+                    return action
+        # If none of the available actions ensure victory, return a random action
+        return random.choice(ava_actions)
+
+
+def play(max_episode=1):
+    start_mark = 'O'
+    env = TicTacToeEnv()
+    agents = [BaseAgent('O'),
+              BaseAgent('X')]
+
+    for _ in range(max_episode):
+        env.set_start_mark(start_mark)
+        state = env.reset()
+        while not env.done:
+            _, mark = state
+            env.show_turn(True, mark)
+            
+            agent = agent_by_mark(agents, mark)
+            ava_actions = env.available_actions()
+            action = agent.act(state, ava_actions)
+            state, reward, done, info = env.step(action)
+            env.render()
+
+        env.show_result(True, mark, reward)
+
+        # rotate start
+        start_mark = next_mark(start_mark)
+
+
+if __name__ == '__main__':
+    play()
